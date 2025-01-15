f32c9586aa023b2f539ba1d1c02d9be10ffa67f1
viktor-lima <joaolimak10@outlook.com>
2025-01-14 23:38:35 -0300

    feat: history, table styles

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,9 +1,60 @@
-import { HistoryContainer } from "./styles";
+import { HistoryContainer, HistoryList } from "./styles";
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>History</h1>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
     </HistoryContainer>
   )
 }
