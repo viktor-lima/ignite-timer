7d24e9f3a13885b05630ab77b9f84aa633e1dd68
viktor-lima <joaolimak10@outlook.com>
2025-01-15 22:18:06 -0300

    feat: learning about controller

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,16 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+import { useState } from "react";
+
+// controller / uncontroller
 
 export function Home() {
+  const [task, setTask] = useState('');
+
+  function resetForm() {
+    setTask('')
+  }
+
   return (
     <HomeContainer>
       <form action="">
@@ -11,6 +20,8 @@ export function Home() {
             id="task" 
             placeholder="De um nome para o seu projeto" 
             list="task-suggestions"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
           />
 
           <datalist id="task-suggestions">
@@ -41,7 +52,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountDownButton disabled type="submit">
+        <StartCountDownButton disabled={!task.trim()} type="submit">
           <Play size={24}/>
           Começar
         </StartCountDownButton>
